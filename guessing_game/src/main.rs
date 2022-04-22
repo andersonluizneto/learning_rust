e4c0eaad8f5194798ab67aad74c58f5e3daf4f30
Anderson Luiz Neto <anderson.neto26@gmail.com>
2022-04-22 20:56:11 -0300

    Finished code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,35 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::{io, num};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o Número!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("Digite seu palpite: ");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha ao ler a linha");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        print!("Você adivinhou: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito menor!"),
+            Ordering::Greater => println!("Muito maior!"),
+            Ordering::Equal => {
+                println!("Você venceu!");
+                break;
+            }
+        }
+    }
 }
