bd8c864dd536c0c6756dd5b456de3e3b43ee6deb
Anderson Luiz Neto <anderson.neto26@gmail.com>
2024-02-10 14:55:15 -0300

    Resolution Curso Rust Udmey-somadois números

--- resolution/src/main.rs
+++ resolution/src/main.rs
@@ -1,4 +1,23 @@
+use std::io;
+
+fn convert_to_int(data_input: & String) -> i32 {
+    let x = data_input.trim().parse::<i32>().unwrap();
+    x
+}
+
 fn main() {
-    let name: &str = "Anderson";
-    println!("Olá {}", name);
+    
+    let mut number1 = String::new();
+    io::stdin().read_line(&mut number1).expect("Error reading number1");
+    
+    let mut number2 = String::new();                                      
+    io::stdin().read_line(&mut number2).expect("Error reading number2");
+    
+    if convert_to_int(&number1) > convert_to_int(&number2){
+        println!("O número {} é maior que {}", number1, number2);
+    }
+    else {
+        println!("O número {} e menor ou igual a {}", number1, number2);
+    }
+
 }
